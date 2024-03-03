70bfc6301e8d489351bab9ff217beb5ad6d38151
RCOOOOOOOOPh <powerfan233@gmail.com>
2024-03-02 16:10:40 -0800

    modified vera and dora code but seems still some problem with dora

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ECE271B
+
+this is UCSD ECE271B 24WI project about LoRA, VeRA and DoRA
--- mylora.py
+++ mylora.py
@@ -47,7 +47,7 @@ class VeRALinear(nn.Linear, lora.LoRALayer):
 
     def reset_parameters(self):
         nn.Linear.reset_parameters(self)
-        if hasattr(self, 'lora_A'):
+        if hasattr(self, 'lora_b'):
             # initialize B the same way as the default for nn.Linear and A to zero
             # this is different than what is described in the paper but should not affect performance
             nn.init.kaiming_uniform_(self.A, a=math.sqrt(5))
@@ -63,13 +63,13 @@ class VeRALinear(nn.Linear, lora.LoRALayer):
             if self.merge_weights and self.merged:
                 # Make sure that the weights are not merged
                 if self.r > 0:
-                    self.weight.data -= T(self.lora_B @ self.lora_A) * self.scaling
+                    self.weight.data -= T((self.lora_b * self.B) @ (self.lora_d * self.A)) * self.scaling
                 self.merged = False
         else:
             if self.merge_weights and not self.merged:
                 # Merge the weights and mark it
                 if self.r > 0:
-                    self.weight.data += T(self.lora_B @ self.lora_A) * self.scaling
+                    self.weight.data += T((self.lora_b * self.B) @ (self.lora_d * self.A)) * self.scaling
                 self.merged = True       
 
     def forward(self, x: torch.Tensor):
@@ -79,5 +79,86 @@ class VeRALinear(nn.Linear, lora.LoRALayer):
             result = F.linear(x, T(self.weight), bias=self.bias)            
             result += (self.lora_dropout(x) @ (self.lora_d * self.A).transpose(0, 1) @ (self.lora_b * self.B).transpose(0, 1)) * self.scaling
             return result
+        else:
+            return F.linear(x, T(self.weight), bias=self.bias)
+        
+class DoraLinear(nn.Linear, lora.LoRALayer):
+    # Dora implemented in a dense layer
+    def __init__(
+        self, 
+        in_features: int, 
+        out_features: int, 
+        r: int = 0, 
+        lora_alpha: int = 1, 
+        lora_dropout: float = 0.,
+        fan_in_fan_out: bool = False, # Set this to True if the layer to replace stores weight like (fan_in, fan_out)
+        merge_weights: bool = True,
+        d_init = 1.0,
+        **kwargs
+    ):
+        nn.Linear.__init__(self, in_features, out_features, **kwargs)
+        lora.LoRALayer.__init__(self, r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout,
+                           merge_weights=merge_weights)
+
+        self.fan_in_fan_out = fan_in_fan_out
+        self.d_init = d_init
+        # Actual trainable parameters
+        if r > 0:
+            '''self.lora_A = nn.Parameter(self.weight.new_zeros((r, in_features)))
+            self.lora_B = nn.Parameter(self.weight.new_zeros((out_features, r)))'''
+            self.lora_A = nn.Parameter(self.weight.new_zeros((r, in_features)))
+            self.lora_B = nn.Parameter(self.weight.new_zeros((out_features, r)))
+            self.lora_m = nn.Parameter(self.weight.norm(p=2, dim=0, keepdim=True))#(1, in_features)
+            self.scaling = self.lora_alpha / self.r
+            self.delta_weight = self.weight.new_zeros((out_features, in_features))
+            # Freezing the pre-trained weight matrix
+            self.weight.requires_grad = False
+        self.reset_parameters()
+        if fan_in_fan_out:
+            self.weight.data = self.weight.data.transpose(0, 1)
+
+    def reset_parameters(self):
+        nn.Linear.reset_parameters(self)
+        if hasattr(self, 'lora_A'):
+            # initialize B the same way as the default for nn.Linear and A to zero
+            # this is different than what is described in the paper but should not affect performance
+            nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+            nn.init.zeros_(self.lora_B)
+
+    def train(self, mode: bool = True):
+        def T(w):
+            return w.transpose(0, 1) if self.fan_in_fan_out else w
+        nn.Linear.train(self, mode)
+        if mode:
+            if self.merge_weights and self.merged:
+                # Make sure that the weights are not merged
+                if self.r > 0:
+                    self.weight.data -= self.delta_weight
+                self.merged = False
+        else:
+            if self.merge_weights and not self.merged:
+                # Merge the weights and mark it
+                if self.r > 0:
+                    loraresult = self.lora_A.transpose(0, 1)  @ self.lora_B.transpose(0, 1) #(in_features, out_features)
+                    numerator = self.weight + self.lora_alpha * loraresult.T #(out_features, in_features)
+                    denominator = numerator.norm(p=2, dim=0, keepdim=True)
+                    directional_component = numerator / denominator
+                    self.delta_weight = self.lora_m * directional_component
+                    self.weight.data += self.delta_weight
+                self.merged = True       
+
+    def forward(self, x: torch.Tensor):
+        def T(w):
+            return w.transpose(0, 1) if self.fan_in_fan_out else w
+        if self.r > 0 and not self.merged:
+            '''result = F.linear(x, T(self.weight), bias=self.bias)            
+            result += (self.lora_dropout(x) @ (self.lora_d * self.A).transpose(0, 1) @ (self.lora_b * self.B).transpose(0, 1)) * self.scaling
+            return result'''
+            loraresult = self.lora_A.transpose(0, 1)  @ self.lora_B.transpose(0, 1) #(in_features, out_features)
+            numerator = self.weight + self.lora_alpha * loraresult.T #(out_features, in_features)
+            denominator = numerator.norm(p=2, dim=0, keepdim=True)
+            directional_component = numerator / denominator
+            new_weight = self.lora_m * directional_component
+            return F.linear(x, new_weight, self.bias)
         else:
             return F.linear(x, T(self.weight), bias=self.bias)
